f446ed10fa8b8ab05d10d2aa0d289414d3b157da
akiJacky <tyatya38an@gmail.com>
2023-12-30 14:54:20 +0900

    init

---
+++ reMind.me/reMind.me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reMind.me
+//
+//  Created by Akihiro Nakano on 2023/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
